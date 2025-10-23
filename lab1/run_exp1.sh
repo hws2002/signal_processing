03285a2d468ebc4778e802639320007a4dbe1b31
“hws2002 <hwooseok123@gmail.com>
2025-10-23 16:21:36 +0800

    fft 포함한 report

--- lab1/run_exp1.sh
+++
@@ -1,7 +0,0 @@
-python exp1.py --N_Fourier 2
-python exp1.py --N_Fourier 4
-python exp1.py --N_Fourier 8
-python exp1.py --N_Fourier 16
-python exp1.py --N_Fourier 32
-python exp1.py --N_Fourier 64
-python exp1.py --N_Fourier 128
--- lab1/run_exp_scipy.sh
+++
@@ -1,7 +0,0 @@
-python exp2_scipy.py --N_Fourier 2
-python exp2_scipy.py --N_Fourier 4
-python exp2_scipy.py --N_Fourier 8
-python exp2_scipy.py --N_Fourier 16
-python exp2_scipy.py --N_Fourier 32
-python exp2_scipy.py --N_Fourier 64
-python exp2_scipy.py --N_Fourier 128
